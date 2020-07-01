31f10fdcb835842ab6f40f9db36683769663c53c
Audrey M <masambaaudrey1@gmail.com>
2020-07-01 09:37:28 +0200

    50. part1  pipes

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Log} from '../../models/Log';
+
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.scss']
 })
 export class LogsComponent implements OnInit {
-//properties with their types
-  logs:{
-    id:string,
-    text: string,
-    date:any,
-
-}[];
+  //properties with their types
+  logs: Log[]; // replace by LOG model
 
   constructor() { }
 
   ngOnInit() {
     this.logs = [
-      {id:'1', text:'Generated components', date:new Date('12/26/2017 12:54:23')},
-      {id:'2', text:'Added bootstrap', date:new Date('12/26/2017 09:33:23')},
-      {id:'3', text:'Added logs components', date:new Date('12/26/2017 05:54:23')},
+      { id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23') },
+      { id: '2', text: 'Added bootstrap', date: new Date('12/26/2017 09:33:23') },
+      { id: '3', text: 'Added logs components', date: new Date('12/26/2017 05:54:23') },
 
     ]
   }
