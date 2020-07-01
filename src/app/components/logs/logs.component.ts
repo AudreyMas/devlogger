3a56525596bc4322a283d5ede88ee7396ad07ab3
Audrey M <masambaaudrey1@gmail.com>
2020-07-01 09:26:16 +0200

    50. ngFor: properties and show the list

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.scss']
 })
 export class LogsComponent implements OnInit {
+//properties with their types
+  logs:{
+    id:string,
+    text: string,
+    date:any,
+
+}[];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.logs = [
+      {id:'1', text:'Generated components', date:new Date('12/26/2017 12:54:23')},
+      {id:'2', text:'Added bootstrap', date:new Date('12/26/2017 09:33:23')},
+      {id:'3', text:'Added logs components', date:new Date('12/26/2017 05:54:23')},
+
+    ]
   }
 
 }
